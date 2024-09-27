2ceb1f882c7318287c8b6a8e987ccd80c835073b
ShawnYS-codemtl <syatsin@gmail.com>
2024-09-27 16:36:25 -0400

    inserts 'other' genre into database and adds it to genres list, render genres in new.ejs

--- index.js
+++ index.js
@@ -45,8 +45,11 @@ app.get("/", async (req, res) => {
     });
 });
 
+
 app.get("/new", async (req, res) => {
-    res.render("new.ejs");
+    const result = await db.query("SELECT name FROM genre ORDER BY name ASC");
+    const genres = result.rows.map(row => row.name);  // Extract the genre names
+    res.render("new.ejs", { genres });
 });
 
 // Handle form submission
@@ -60,14 +63,20 @@ app.post('/review', async (req, res) => {
       thoughts: req.body.thoughts,
       summary: req.body.summary,
       image: req.body.image,
-      rating: req.body.rating
+      rating: req.body.rating, 
+      other: req.body.other 
     };
     console.log(req.body);
     const result = await db.query("INSERT INTO anime (title, post_date, season, release_year, personal_thoughts, summary, image, rating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
          [newReview.title, newReview.date, newReview.season, newReview.year, newReview.thoughts, newReview.summary, newReview.image, newReview.rating]);
     const id = result.rows[0].id;
+    const genres = [req.body.genres];
+    if (newReview.other) {
+        await db.query("INSERT INTO genre (name) VALUES($1)", [newReview.other]);
+        genres.push(newReview.other);
+        // insert into new.ejs form a new option with value and label of the 'other' genre
+    }
     console.log(req.body.genres);
-    const genres = req.body.genres;
     genreToAnime(genres, id);
     res.redirect("/");
   });
