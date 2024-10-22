1c80a0810a24e760e1c98136d413fe0b7b52028f
ShawnYS-codemtl <syatsin@gmail.com>
2024-10-22 16:41:38 -0400

    added the delete function

--- index.js
+++ index.js
@@ -73,17 +73,41 @@ app.get('/edit/:id', async (req, res) => {
     }
 });
 
-// Route to handle the edit form submission
-app.post('/edit/:id', (req, res) => {
-    const postId = req.params.id;
-    const postIndex = posts.findIndex(p => p.id == postId);
-    if (postIndex !== -1) {
-        posts[postIndex].title = req.body.title;
-        posts[postIndex].content = req.body.content;
-        res.redirect('/');
-    } else {
-        res.status(404).send('Post not found');
+// Route to handle the edit form submission    * need to do, update all changes in database 
+app.post('/edit/:id', async (req, res) => {   
+    const postId = req.params.id; 
+    const updatedReview = {
+        title: req.body.title,
+        date: req.body.date,
+        season: req.body.season,
+        year: req.body.year,
+        thoughts: req.body.thoughts,
+        platform: req.body.platform,
+        episodes: req.body.episodes,
+        summary: req.body.summary,
+        image: req.body.image,
+        rating: req.body.rating, 
+        other: req.body.other 
+      };
+    const result = await db.query("UPDATE anime SET title=$1, post_date=$2, season=$3, release_year=$4, personal_thoughts=$5, platform=$6, nb_episodes=$7, summary=$8, image=$9, rating=$10 WHERE id=$11",
+        [updatedReview.title, updatedReview.date, updatedReview.season, updatedReview.year, updatedReview.thoughts, updatedReview.platform, updatedReview.episodes, updatedReview.summary, updatedReview.image, updatedReview.rating, postId]
+    );
+    const genres = [req.body.genres];
+    if (updatedReview.other && !genres.includes(updatedReview.other)) {
+        await db.query("INSERT INTO genre (name) VALUES($1)", [newReview.other]);
+        genres.push(updatedReview.other);
+        // insert into new.ejs form a new option with value and label of the 'other' genre
     }
+    genreToAnime(genres, postId);
+    res.redirect("/");
+});
+
+// Route to handle deleting a post
+app.post('/delete/:id', async (req, res) => {
+    const postId = req.params.id;
+    const result1 = await db.query("DELETE from anime_genre WHERE aid=$1",[postId]); 
+    const result2 = await db.query("DELETE from anime WHERE id=$1", [postId]);
+    res.redirect('/');
 });
 
 // Handle form submission
@@ -94,6 +118,8 @@ app.post('/review', async (req, res) => {
       date: req.body.date,
       season: req.body.season,
       year: req.body.year,
+      platform: req.body.platform,
+      episodes: req.body.episodes,
       thoughts: req.body.thoughts,
       summary: req.body.summary,
       image: req.body.image,
@@ -101,8 +127,8 @@ app.post('/review', async (req, res) => {
       other: req.body.other 
     };
     //console.log(req.body);
-    const result = await db.query("INSERT INTO anime (title, post_date, season, release_year, personal_thoughts, summary, image, rating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id",
-         [newReview.title, newReview.date, newReview.season, newReview.year, newReview.thoughts, newReview.summary, newReview.image, newReview.rating]);
+    const result = await db.query("INSERT INTO anime (title, post_date, season, release_year, personal_thoughts, summary, image, rating, platform, nb_episodes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id",
+         [newReview.title, newReview.date, newReview.season, newReview.year, newReview.thoughts, newReview.summary, newReview.image, newReview.rating, newReview.platform, newReview.episodes]);
     const id = result.rows[0].id;
     const genres = [req.body.genres];
     if (newReview.other) {
